a78ec9a1a8d96b663c8fb8fa765c606c01494e7f
marcin <mk.grzesiak@gmail.com>
2023-01-13 21:56:53 +0100

    Check for natural number done!

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,7 +1,39 @@
 package numbers;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-//        write your code here
+    public  static  void main(String[] args) {
+        String passedNumber;
+        AllMyMethods obiekt = new AllMyMethods();
+        passedNumber=obiekt.enterNumber();
+        obiekt.checkNaturalNumber(passedNumber);
+
+
+
+
+
+
+    }
+
+    static class AllMyMethods{
+
+
+
+         String enterNumber (){
+            Scanner scan=new Scanner(System.in);
+            System.out.println("Enter a natural number:");
+            return  scan.next();
+        }
+
+        void checkNaturalNumber(String putNumber){
+             int checkThis = Integer.parseInt(putNumber);
+
+             if(checkThis<=0){
+                 System.out.println("This number is not natural!");
+                 System.exit(1);
+             }
+        }
+
     }
 }
