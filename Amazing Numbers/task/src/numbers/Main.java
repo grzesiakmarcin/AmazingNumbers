7039a0ef8f47f84cf52db4e92bc6fc5f1b033b5b
marcin <mk.grzesiak@gmail.com>
2023-01-19 07:29:21 +0100

    StringBuilder added.

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -4,60 +4,102 @@ import java.util.Scanner;
 
 public class Main {
     public  static  void main(String[] args) {
+        String[] tableOfAnswers= new String[4];
         String passedNumber;
+
         AllMyMethods obiekt = new AllMyMethods();
         passedNumber=obiekt.enterNumber();
-        obiekt.checkNaturalNumber(passedNumber);
-        obiekt.checkEven(passedNumber);
+        obiekt.checkNaturalNumber(passedNumber,tableOfAnswers);
+        obiekt.checkEven(passedNumber,tableOfAnswers);
+        obiekt.checkDivisible(passedNumber,tableOfAnswers);
+        obiekt.checkLastDig(passedNumber,tableOfAnswers);
+        obiekt.printAllinfo(tableOfAnswers);
 
 
 
     }
 
     static class AllMyMethods{
+        void printAllinfo(String[] tab){
+            for (String element:tab
+                 ) {
+                System.out.println(element);
+
+            }
+
+
+        }
+
 
 
 
          String enterNumber (){
             Scanner scan=new Scanner(System.in);
-            System.out.println("Enter a natural number:");
+            System.out.println("Enter natural number");
             return  scan.next();
         }
 
-        void checkNaturalNumber(String putNumber){
+        String[] checkNaturalNumber(String putNumber,String[]tableOfAnswers){
              int checkThis = Integer.parseInt(putNumber);
 
              if(checkThis<=0){
                  System.out.println("This number is not natural!");
                  System.exit(1);
              }
-        }
-
-        void checkLastDig(String putNumber){
-             int lenght = putNumber.length();
-             char[] table = putNumber.toCharArray();
-             if(table.length-1==7){
-                 System.out.println(putNumber = " ends with 7");
+             else {tableOfAnswers[2]="Explanation:";
+             tableOfAnswers[3]=putNumber+ " ";
              }
+             return tableOfAnswers;
+
 
         }
 
-
-        void checkDivisible(String putNumber){
+        String[] checkDivisible(String putNumber,String [] tableOfAnswers){
+            StringBuilder sb= new StringBuilder();
 
             if(Integer.parseInt(putNumber) % 7 == 0){
-                System.out.println(putNumber = " is divisible 7");
+                tableOfAnswers[1]="It is a Buzz number.";
+                tableOfAnswers[3]=  String.valueOf(sb.append(tableOfAnswers[3] + "is divisible by 7"));
             }
+            else{
+                tableOfAnswers[1]="It is not a Buzz number.";
+                tableOfAnswers[3]= String.valueOf(sb.append(tableOfAnswers[3] + " is not divisible by 7"));
+            }
+            return tableOfAnswers;
 
         }
 
-        void checkEven(String putNumer){
-             if(Integer.parseInt(putNumer)%2==0){
-                 System.out.println("This number is Odd.");
+        String[] checkLastDig(String putNumber, String[] tablOfAnswers){
+            StringBuilder sb = new StringBuilder();
+            Integer.parseInt(putNumber);
+             char[] table = String.
+
+                int counter = 1;
+            for (char element:table             ) {
+                System.out.println(element+ "element " + counter);
+                counter++;
+            }
+
+             if(table.length-1=='7'){
+                 tablOfAnswers[1]="It is a Buzz number.";
+                 tablOfAnswers[3]=String.valueOf(sb.append(tablOfAnswers[3]+" ends with 7"));
              }
              else {
-                 System.out.println("This number is Even.");
+                 tablOfAnswers[3]=String.valueOf(sb.append(tablOfAnswers[3]+" nor ends with 7"));
              }
+             return tablOfAnswers;
+
+        }
+
+
+        String[] checkEven(String putNumer,String[] tableOfAnswers ){
+             if(Integer.parseInt(putNumer)%2==0){
+                 tableOfAnswers[0]=("This number is Odd.");
+             }
+             else {
+                 tableOfAnswers[0]=("This number is Even.");
+             }
+             return  tableOfAnswers;
         }
     }
 }
